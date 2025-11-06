19a281f5d6a79e622117f2c5a3fdc64e47cd2eda
Luiz Felipe Dias de Araujo <141677885+LuizFelipeDias@users.noreply.github.com>
2025-11-06 01:17:38 -0300

    Fix login route and update authenticated user route

--- back/laravel/routes/web.php
+++ back/laravel/routes/web.php
@@ -11,15 +11,10 @@ Route::get('/', [IndexController::class, 'index']);
 
 // Login e logout (guard web, sessão)
 Route::post('/login',  [LoginController::class, 'login']);
-<<<<<<< HEAD
 Route::get('/login', [IndexController::class, 'index'])->name('login');
-=======
-Route::get('/login', [IndexController::class, 'index'])->name('login'); // << AQUI
->>>>>>> 479b437a945bea48db3995dafac77e966fcf7320
 
 Route::post('/logout', [LogoutController::class, 'perform']);
 
 // Rota opcional para testar usuário autenticado via sessão
-Route::get('/me', fn (\Illuminate\Http\Request $r) => $r->user())
+Route::get('/me', fn (Request $r) => $r->user())
     ->middleware('auth:web'); // ou só ->middleware('auth')
-
